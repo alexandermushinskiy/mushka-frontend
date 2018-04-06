99c347b519f3a731264260dfaf429cb40ac05cbc
alexandermushinskiy <amushinskiy@gmail.com>
2018-04-07 02:20:44 +0300

    refactor tables

--- src/app/products/products-table/products-table.component.ts
+++ src/app/products/products-table/products-table.component.ts
@@ -76,7 +76,6 @@ export class ProductsTableComponent extends PsaDatatableComponent implements OnI
 
   sort() {
     const sort = this.sortColumn();
-    this.sorts = [sort];
     this.rowsData = this.onTableSort({ sorts: [{ ...{ initSort: true }, ...sort }] }, this.initialRowsData);
   }
 
